b7d9a9b3dacf137238ef7d50f7b0de84964b2b8a
tristan-tsl <tanshilinmail@gmail.com>
2020-03-25 04:20:44 +0800

    add the rmi docker images doc

--- dependency/laasops.sh
+++ dependency/laasops.sh
@@ -3,6 +3,7 @@ mv application.yml /data/tristan/laashub/laasops/distribution/configs/applicatio
 
 docker stop laasops
 docker rm laasops
+docker rmi laashub/laasops
 
 docker run -d --name laasops -p 5000:5000 -v /data/tristan/laashub/laasops/distribution/configs:/usr/src/app/distribution/configs  laashub/laasops
 docker logs -f --tail 100 laasops
