865fabd167641a62b22f78883fba75e90591023d
tristan-tsl <tanshilinmail@gmail.com>
2020-03-25 04:42:11 +0800

    temp

---
+++ engine/context.py
@@ -0,0 +1,13 @@
+"""
+上下文
+"""
+
+
+# 数据
+def data():
+    pass
+
+
+# 运行环境
+def runtime():
+    pass
---
+++ engine/gateway.py
@@ -0,0 +1,18 @@
+"""
+网关
+"""
+
+
+# 路由
+def route():
+    pass
+
+
+# 注册
+def register():
+    pass
+
+
+# 调用
+def call():
+    pass
